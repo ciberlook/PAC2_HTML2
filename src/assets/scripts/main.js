6731996c10235dad8d2fb534ce0871469460d52a
Alberto Marín Mendoza <amarinme@uoc.edu>
2023-12-06 11:00:11 +0100

    fin de la pac2

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -5,27 +5,27 @@
 
 // import 'some-node-module';
 // import SomeModule from 'some-node-module';
-import * as bootstrap from 'bootstrap';
+import * as bootstrap from "bootstrap";
 
 /**
  * Write any other JavaScript below
  */
 
-const mButton = document.querySelector('.sandwich');
-const menu = document.querySelector('.menu');
+const mButton = document.querySelector(".sandwich");
+const menu = document.querySelector(".menu");
 
 mButton.addEventListener(
-  'click',
+  "click",
   (e) => {
     if (
-      e.target.classList[0] === 'sandwich__bar1' ||
-      e.target.classList[0] === 'sandwich__bar2' ||
-      e.target.classList[0] === 'sandwich__bar3'
+      e.target.classList[0] === "sandwich__bar1" ||
+      e.target.classList[0] === "sandwich__bar2" ||
+      e.target.classList[0] === "sandwich__bar3"
     )
-      e.target.parentElement.classList.toggle('change');
-    else e.target.classList.toggle('change');
+      e.target.parentElement.classList.toggle("sandwich--change");
+    else e.target.classList.toggle("sandwich--change");
 
-    menu.classList.toggle('menu--hidden');
+    menu.classList.toggle("menu--hidden");
   },
   false,
 );
